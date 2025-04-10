2e69b55738f03a4a53ebd18aeb357ce85ac5b463
Nj221102 <nitishj221102@gmail.com>
2025-04-11 04:39:13 +0530

    domain config

--- next.config.js
+++ next.config.js
@@ -6,11 +6,9 @@ const nextConfig = {
   // Only use export for production builds
   ...(process.env.NODE_ENV === 'production' ? {
     output: 'export',  // Required for GitHub Pages static export
-    basePath: process.env.GITHUB_ACTIONS ? '/shopstr-gl-frontend' : '',
     images: {
       unoptimized: true, // Required for static export
     },
-    assetPrefix: process.env.GITHUB_ACTIONS ? '/shopstr-gl-frontend' : '',
   } : {}),
   
   eslint: {
